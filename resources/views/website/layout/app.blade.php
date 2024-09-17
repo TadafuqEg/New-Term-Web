52878d74c38009730ebc544341e4ffa9a2ce72a3
fathysamir <01154857080fa@gmail.com>
2024-09-17 16:37:16 +0300

    Initial commit

--- resources/views/website/layout/app.blade.php
+++ resources/views/website/layout/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta property="og:title" content="Term">
     <meta property="og:description" content="Listen, Respond, Succeed">
     <meta property="og:image" content="{{asset('website/assets/imgs/term_share02.png')}}">
