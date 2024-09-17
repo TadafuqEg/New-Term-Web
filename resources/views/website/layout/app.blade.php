d65ef0797ddb1e5e63c21ce714be7dfbc533cceb
fathysamir <01154857080fa@gmail.com>
2024-09-17 16:40:00 +0300

    Initial commit

--- resources/views/website/layout/app.blade.php
+++ resources/views/website/layout/app.blade.php
@@ -12,7 +12,7 @@
     <meta property="og:image:height" content="630">
     <meta property="og:url" content="https://dev.term.ae/">
     <title>@yield('title', 'Term')</title>
-    <link rel="icon" href="{{asset('website/assets/imgs/Logo2.svg')}}">
+    <link rel="icon" href="{{asset('website/assets/imgs/logo2.svg')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('website/css/style.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
