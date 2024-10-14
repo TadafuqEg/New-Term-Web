3b8afbebcc117aa206c794c965766bfdc613c4d9
fathysamir <01154857080fa@gmail.com>
2024-10-14 15:19:42 +0300

    Initial commit of Laravel project

--- resources/views/website/sign_up.blade.php
+++ resources/views/website/sign_up.blade.php
@@ -11,6 +11,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W4D7RJ48');</script>
     <style>
         .log-sign{
             background-image: url("{{asset('website/assets/imgs/login.gif')}}");
@@ -184,9 +189,12 @@
             }
         }
     </style>
+    
 </head>
 
 <body>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4D7RJ48"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <div class="head">
         <nav class="navbar">
             <div class="content content2">
