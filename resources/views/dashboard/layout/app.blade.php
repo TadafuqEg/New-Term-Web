302df86366afeb42f8ce58efae9c7449f4556c48
fathysamir <01154857080fa@gmail.com>
2024-10-31 13:39:28 +0300

    Initial commit

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <link rel="icon" type="image/x-icon" href="{{asset('website/assets/imgs/logo2.svg')}}">
+    <link rel="icon" type="image/x-icon" href="{{asset('website/assets/imgs/logo.svg')}}">
     <title>@yield('title', 'Term - Admin Home')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- loader-->
