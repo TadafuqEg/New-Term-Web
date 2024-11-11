9de91ab17e1961471f627f0f207b8272330602ce
fathysamir <01154857080fa@gmail.com>
2024-11-11 14:36:57 +0200

    Initial commit

--- resources/views/website/login.blade.php
+++ resources/views/website/login.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
+        new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=
+        "https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,"script","dataLayer","GTM-W4D7RJ48");
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Term-login</title>
@@ -11,12 +17,7 @@
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
         <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
-        new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=
-        "https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,"script","dataLayer","GTM-W4D7RJ48");
-    </script>
+   
         <!-- End Google Tag Manager -->
     
     <style>
